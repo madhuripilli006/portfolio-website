9232b16a87c26d484722ab9d13fb4c6498b99096
madhuripilli006 <madhu897712@gmail.com>
2026-05-10 15:37:12 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Backend is working");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const contactRoutes = require("./routes/contactRoutes");
+app.use("/api/contact", contactRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
